9f2471ffc9e6655dac9677ad6b9cd177d23cbda5
schneider001 <69435543+schneider001@users.noreply.github.com>
2020-09-11 18:08:53 +0300

    Update quadratic equation.cpp

--- quadratic equation.cpp
+++ quadratic equation.cpp
@@ -1,46 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <iostream>
 
-
-double* get_numbers(double* koef, int size);
+double* get_numbers(int size);
 double find_discrim(double* koef);
-double* find_roots(double* koef, double discrim);
+double* find_roots(int count_of_koef);
 
 int main() {
-	double* koef;
-	double* answer;
 	int count_of_koef = 3;
-	std::cout << "Please, enter coefficients of quadratic equation:" << std::endl;
-	koef = get_numbers(koef, count_of_koef);
-	if (koef[0] != 0) {
-		double discrim = find_discrim(koef);
-		if (discrim < 0){
-		    std::cout << "Equastion doesn't have any root" << std::endl;
-		}
-		else {
-			answer = find_roots(koef, discrim);
-			if (discrim == 0){
-			    std::cout << "Resolve is one root: " << answer[0] << std::endl;
-			}
-			else {
-			    std::cout << "Roots are: " << answer[0] << " and " << answer[1] << std::endl;
-			}
-			free(answer);
-		}
+	double* answer = find_roots(count_of_koef);
+	double count_of_solutions = answer[0];
+	if (count_of_solutions == 0) {
+		printf("No roots!!\n");
+	}
+	else if (count_of_solutions == 1) {
+		printf("There is one root: %lf \n", answer[1]);
 	}
 	else {
-		double linear_root = -koef[2]/koef[1];
-		std::cout << "Equastion is linear! Root: " << linear_root << std::endl;
+		printf("Roots: %lf %lf \n", answer[1], answer[2]);
 	}
-	free(koef);
 }
 
 
-double* get_numbers(double* koef, int size) {
-	koef = (double*)malloc(size* sizeof(double));
+double* get_numbers(int size) {
+	double* koef = (double*)calloc(size, sizeof(double));
 	for (int i = 0; i < size; i++) {
-	    std::cin >> koef[i];
+		printf("koef[%d] = ", i);
+		scanf("%lf", &koef[i]);
 	}
 	return koef;
 }
@@ -50,9 +36,32 @@ double find_discrim(double* koef) {
 	return koef[1] * koef[1] - 4 * koef[0] * koef[2];
 }
 
-double* find_roots(double* koef, double discrim) {
-	double* answer = (double*)malloc(2 * sizeof(double));
-	answer[0] = (-koef[1] + sqrt(discrim)) / (2 * koef[0]);
-	answer[1] = (-koef[1] - sqrt(discrim)) / (2 * koef[0]);
+
+double* find_roots(int count_of_koef) {
+	double* answer = (double*)calloc(count_of_koef, sizeof(double));
+	double* koef = get_numbers(count_of_koef);
+	if (koef[0] != 0){
+		double discrim = find_discrim(koef);
+		if (discrim < 0){
+			answer[0] = 0;
+		}
+		else {
+			double x1 = answer[1] = (-koef[1] + sqrt(discrim)) / (2 * koef[0]);
+			double x2 = answer[1] = (-koef[1] - sqrt(discrim)) / (2 * koef[0]);
+			if (discrim == 0){
+				answer[0] = 1;
+				answer[1] = x1;
+			}
+			else {
+				answer[0] = 2;
+				answer[1] = x1;
+				answer[2] = x2;
+			}
+		}
+	}
+	else {
+		answer[0] = 1;
+		answer[1] = -koef[2]/koef[1];
+	}
 	return answer;
 }
